a3a1ad2b1383fbaaa5c4becae16995a321aaae10
Shubham Shivaji Tangde <shubhamtangde2961@gmail.com>
2025-04-02 06:27:17 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+  "use strict"; //treat all JS code as newer version
+
+  //alert(3 + 3) // we are using nodejs, not browser
+
+  console.log(3+3);
+  console.log("Hitest")
+
+  let name="shubham"
+  let age = 22
+  let isLoggedIn = false
+
+  //number
+  //bigint
+  //string=> ""
+  // boolean => true/false
+  // null => standalone value
+  // undefined =>
+    //symbol => unique
+  // object
+
+  console.log(typeof null);
