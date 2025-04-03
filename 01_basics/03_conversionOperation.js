c67a7d6619c637c912adac9fca09c4db2a65c5be
Shubham Shivaji Tangde <shubhamtangde2961@gmail.com>
2025-04-03 16:02:47 +0000

    coversionOperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "hitesh"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueNumber = Number(score);
-console.log(typeof valueNumber);
-console.log(valueNumber);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
 
 // "33" => 33
 // "33abc" => NaN -> not a number
@@ -14,14 +14,38 @@ console.log(valueNumber);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true ; 0 => false
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*************************** Operations ***************************************
+
+let value = 3
+let negvalue = -value;
+//console.log(negvalue)
+
+let str1 = "Hello"
+let str2 = " Hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+ 2 + 2);
+// console.log(1+2+"2");
+
+// console.log( (3+4) *5 %3);
+
+console.log(+true);
+console.log(+" ");
+
+let num1,num2, num3
