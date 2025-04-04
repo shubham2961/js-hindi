d63af58d3bd700486d7b7b82edc5fd3ddde4f0e9
Shubham Shivaji Tangde <shubhamtangde2961@gmail.com>
2025-04-04 05:53:23 +0000

    summery

---
+++ 01_basics/datatypesSummery.js
@@ -0,0 +1,35 @@
+// primitive
+ // 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+
+ const score = 100
+ const scoreValue = 100.3
+
+ const isLoggedIn = false
+ const outsideTemp = null
+  let userEmail;
+
+  const id = Symbol('123')
+  const anotherId = Symbol('123')
+
+  console.log(id == anotherId);
+ //
+ // Reference (Non primitive)
+
+ // Array, Objects, Functions
+
+ const heros = ["shaktiman","naagraj","doga"];
+ let myobj={
+    name:"hitesh",
+    age:22,
+ }
+ const muFunction = function(){
+   console.log("hello world");
+ }
+
+
+ //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+ // Stack(Primitive), Heap(Non-Primitive)
+
+ let myYoutubename = "hiteshchoudhari.com"
+ 
